1f357dff6d43fc3a0da4ae042c0a4b8f2fe84dea
tobo22 <131508856+tobo22@users.noreply.github.com>
2025-09-16 08:24:21 -0300

    Add files via upload

---
+++ TP4/TP4 ejA (incompleto).cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+void inicio(int &cant, int &juan[]);
+void inicio(int &cant, int &juan[]){
+	
+	int cant;
+	int juan[cant];
+	
+	cout<<"ingrese la cantidad de juanes que ingresara: ";cin>>cant:
+		
+	for(int i = 0; i < cant; i++){
+		cout<<"ingrese su "<<i+1<<" juan: ";cin>>juan[i]:
+	}
+	
+}
+
+void encontrar(int cant, int juan[]);
+void encontrar(){
+	
+	suma = 0;
+	prom
+	
+	for(int i = 0; i < cant; i++){
+		
+		suma=suma+juan[i];
+	}
+
+}
+
+int main(){
+	
+	int cant;
+	int juan[cant];
+	
+	inicio(&cant, &juan[]);
+	a(cant, juan[]);
+	e();
+	
+	
+}
